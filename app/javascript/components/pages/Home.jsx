ea96c018c0109c6b7d6e981981852b57256483ee
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-10-28 13:10:23 +0100

    react app setup

---
+++ app/javascript/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Nested Notes</h1>
+        <p className="lead">
+          A place to store your notes
+        </p>
+        <hr className="my-4" />
+      </div>
+    </div>
+  </div>
+);
