c5219e945c8750fde9f1fac7d762e2fada93c12a
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-10-28 12:56:03 +0100

    hello react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
-  get 'homepage/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+Rails
+  .application
+  .routes
+  .draw do
+    root 'homepage#index'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
