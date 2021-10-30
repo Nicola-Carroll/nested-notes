41741f8578f43401a997f404f2ebf134814b1689
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-10-30 16:33:24 +0100

    add child note seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+notes = [
+  { title: 'Hello!', body: 'This is an example note' },
+  { title: 'Shopping list', body: 'Apples, Cereal, Snacks' },
+  {
+    title: 'Reading list',
+    body: 'Katie Price authobiography, the dictionary, my revision notes'
+  },
+  { title: 'Hello +1 !', body: 'This is a child note', parent_id: 1 }
+]
+
+notes.each { |note| Note.create(note) }
